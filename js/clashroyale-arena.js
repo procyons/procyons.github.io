e9e97387f63d7cd1abc0c2ff29d5b1c733ef8835
Everton Amaral <everton.amaral@gmail.com>
2017-03-16 14:15:46 -0300

    2cup

--- js/clashroyale-arena.js
+++ js/clashroyale-arena.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
             });
     }
 
-        var endDate = "march 3, 2017 00:00:00";
+        var endDate = "march 24, 2017 00:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
